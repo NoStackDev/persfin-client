6215084083c5421dbecc0b3d22b5f705ab393fb3
zino <partialstackdev@gmail.com>
2023-01-28 16:59:02 +0100

    fix: send create inflow mutation after successful budget mutation

--- src/Mutations/Outflow/AddOutflow.tsx
+++ src/Mutations/Outflow/AddOutflow.tsx
@@ -13,44 +13,15 @@ const AddOutflow = async (
   category: string,
   description: string
 ) => {
-  console.log({ title, amount, budget, item, category, description });
-  try {
-    if (budget) {
-      // const updateBudgetMutation = UpdateBudget();
-      const budgetItem = budget.items.map((obj: BudgetItemType) => {
-        if (obj.id !== item.id) return obj;
-        obj.balance = obj.balance - amount;
-        return obj;
-      });
-      // updateBudgetMutation.mutate({
-      //   budgetId: budget.id,
-      //   title: budget.title,
-      //   total: budget.total,
-      //   balance: budget.balance - amount,
-      //   description: budget.description,
-      //   items: JSON.stringify(budget.items),
-      // });
-      console.log({
-        budgetId: budget.id,
-        title: budget.title,
-        total: budget.total,
-        balance: budget.balance - amount,
-        description: budget.description,
-        items: budget.items,
-      });
-    }
-    // return pb.collection("outflows").create({
-    //   title,
-    //   amount,
-    //   budget,
-    //   item,
-    //   category,
-    //   description,
-    //   user: pb.authStore.model?.id,
-    // });
-  } catch (err: any) {
-    console.log(err.message);
-  }
+  return pb.collection("outflows").create({
+    title,
+    amount,
+    budget,
+    item,
+    category,
+    description,
+    user: pb.authStore.model?.id,
+  });
 };
 
 type Args = {
@@ -74,8 +45,8 @@ const CreateOutflow = () => {
       category,
     }: Args) => AddOutflow(title, amount, budget, item, category, description),
     onSuccess: () => {
-      queryClient.invalidateQueries("outflows");
-      queryClient.invalidateQueries("budgets");
+      queryClient.invalidateQueries(["outflows"]);
+      queryClient.invalidateQueries(["budgets"]);
     },
   });
 };
