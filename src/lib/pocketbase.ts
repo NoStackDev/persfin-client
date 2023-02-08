8e6bf53eeead6ac28c2e781ac901adc0737da16a
zino <partialstackdev@gmail.com>
2023-02-08 19:26:41 +0100

    fix: revert pocket base url to env

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("https://tall-addition.pockethost.io");
+const pb = new PocketBase(process.env.PB_URL || process.env.REACT_APP_PB_URL);
 
 export default pb;
