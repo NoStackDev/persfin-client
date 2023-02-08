f16ec08dbbf092ac81216a01f611c40ad7f15a62
zino <partialstackdev@gmail.com>
2023-02-08 20:31:04 +0100

    fix: working on fix for netlify env

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,5 +1,7 @@
 import PocketBase from "pocketbase";
 
+console.log(process.env.PB_URL || process.env.REACT_APP_PB_URL);
+
 const pb = new PocketBase(process.env.PB_URL || process.env.REACT_APP_PB_URL);
 
 export default pb;
