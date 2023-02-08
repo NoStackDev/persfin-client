b4b2c736285cfd41e47237e41fc9805e1ceb816d
zino <partialstackdev@gmail.com>
2023-02-08 20:33:38 +0100

    fix: remove unncessary console log in lib/pb

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,5 @@
 import PocketBase from "pocketbase";
 
-console.log(process.env.PB_URL || process.env.REACT_APP_PB_URL);
-
 const pb = new PocketBase(process.env.PB_URL || process.env.REACT_APP_PB_URL);
 
 export default pb;
