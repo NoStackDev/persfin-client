a670dc68252b780510ecd5285c4c2f10c9182c8f
zino <partialstackdev@gmail.com>
2023-02-08 19:21:08 +0100

    fix: trying db in public

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase(process.env.REACT_APP_PB_URL);
+const pb = new PocketBase("https://tall-addition.pockethost.io");
 
 export default pb;
